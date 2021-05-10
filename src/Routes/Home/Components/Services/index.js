491a8b0d1fe5f55e2213287c2b5d696f82054f9f
kuldeep <kuldeepdeveloperxkodestudio@gmail.com>
2021-05-10 12:25:28 +0530

    init

---
+++ src/Routes/Home/Components/Services/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.scss';
+
+const Services = () =>{
+    return(
+        <>
+            <div className="">
+                <div className=""></div>
+
+            </div>
+        </>
+    )
+}
+export default Services;
