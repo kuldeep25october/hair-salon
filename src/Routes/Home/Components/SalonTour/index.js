89f6d5010dc3a493e5f9247724b0d6aa065c40d3
kuldeep <kuldeepdeveloperxkodestudio@gmail.com>
2021-05-12 13:19:12 +0530

    init

--- src/Routes/Home/Components/SalonTour/index.js
+++ src/Routes/Home/Components/SalonTour/index.js
@@ -21,7 +21,7 @@ const Tour = () =>{
             </li>
             </ul>
             </div>
-            <div class="ReactYTPlayer col-lg-8">
+            <div className="ReactYTPlayer col-lg-8">
             <div style={{float: 'right', width: '640px', height: '360px'}}>
             <div style={{width: '100%', height: '100%'}}>
             <iframe frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" width="100%" height="100%" src="https://www.youtube.com/embed/uO1RnThE-Eg?autoplay=0&amp;mute=0&amp;controls=0&amp;origin=http%3A%2F%2F192.168.1.21%3A3000&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;iv_load_policy=3&amp;modestbranding=1&amp;enablejsapi=1&amp;widgetid=1" id="widget2"></iframe>
