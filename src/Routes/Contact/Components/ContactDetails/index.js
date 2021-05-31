9cea8e71a5d92843d3052d91748cbc013c0b5868
kuldeep <kuldeepdeveloperxkodestudio@gmail.com>
2021-05-31 18:42:22 +0530

    init

--- src/Routes/Contact/Components/ContactDetails/index.js
+++ src/Routes/Contact/Components/ContactDetails/index.js
@@ -9,47 +9,42 @@ const ContactDetails = () => {
             <div className="ContactDetails">
                 <Row>
                     <Col lg='6'>
-                                <h1>Get In Touch</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
-                                <address className="ContactInfo">
-
-                                <EnvironmentOutlined />
-                                <h2>Address</h2>
-                                <p>27 Division St, New York, USA</p><br/>
-
-                                <PhoneOutlined />
-                                <h2>Phone</h2>
-                                <p>1-677-124-44227</p><br/>
-                                <PhoneOutlined />
-                                <h2>Phone</h2>
-                                <p>1-677-124-44228</p><br/>
-                                <MailOutlined />
-                                <h2>Email</h2>
-                                <p>email@salon.com</p><br/>
-                                <MailOutlined />
-                                <h2>Email</h2>
-                                <p>support@salon.com</p><br/>
-                                </address>
-             
+                        <h1>Get In Touch</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                        <address className="ContactInfo">
+                            <EnvironmentOutlined />
+                            <h2>Address</h2>
+                            <p>27 Division St, New York, USA</p>
+                            <PhoneOutlined />
+                            <h2>Phone</h2>
+                            <p>1-677-124-44227</p>
+                            <PhoneOutlined />
+                            <h2>Phone</h2>
+                            <p>1-677-124-44228</p>
+                            <MailOutlined />
+                            <h2>Email</h2>
+                            <p>email@salon.com</p>
+                            <MailOutlined />
+                            <h2>Email</h2>
+                            <p>support@salon.com</p>
+                        </address>
                     </Col>
                     <Col lg='6'>
                         <div className='contactForm'>
                             <p>Your Name (required)</p>
-                            <input type="text" /><br/><br/>
+                            <input type="text" />
                             <p>Your Email (required)</p>
-                            <input type="text" /><br/><br/>
+                            <input type="text" />
                             <p>Subject</p>
-                            <input type="text" /><br/><br/>
+                            <input type="text" />
                             <p>Your Message</p>
-                            <input id='formMsg' type="text" /><br/>
-                            <input id='sbmtBtn' type="button" value="SEND"/>
+                            <textarea className='formMsg' rows="10" cols="30" />
+                            <input id='sbmtBtn' type="button" value="SEND" />
                         </div>
                     </Col>
-
                 </Row>
             </div>
         </>
     )
-
 }
 export default ContactDetails;
